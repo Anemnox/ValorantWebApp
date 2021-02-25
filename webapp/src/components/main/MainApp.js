b5113b664f21bbd8608c4412d486841718fb578c
Andrew <anemnox@gmail.com>
2021-02-25 15:59:21 -0800

    Starting on Webapp

---
+++ webapp/src/components/main/MainApp.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./mainapp.css";
+import NavBar from "../navbar/NavBar.js";
+
+const MainApp = (props) => {
+  return (
+    <div className="mainapp">
+      <NavBar />
+    </div>
+  );
+}
+
+export default MainApp;
---
+++ webapp/src/components/navbar/NavBar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./navbar.css";
+
+class NavBar extends React.Component {
+
+  render() {
+      return (
+        <div className="navbar">
+          NavBar
+        </div>
+      );
+  }
+}
+
+export default NavBar;
