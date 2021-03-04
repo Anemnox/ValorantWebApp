d8eb93092f025735fa6d86eadef5277c78f1c250
Andrew <anemnox@gmail.com>
2021-03-03 19:29:07 -0800

    Added socket io to client

--- webapp/src/components/main/MainApp.js
+++ webapp/src/components/main/MainApp.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./mainapp.css";
 import NavBar from "../navbar/NavBar.js";
 
+const sio = require("socket.io-client")("http://localhost:4000");
+
 const MainApp = (props) => {
+  const [message, setMsg] = useState("Hello World!");
+
+  sio.on("Message", (msg) => {
+    setMsg(msg);
+  });
+
   return (
     <div className="mainapp">
       <NavBar />
+      {message}
     </div>
   );
 }
